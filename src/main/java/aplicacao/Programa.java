bb2d32112eaa747ff0452d131fd57664fa0973b3
Douglas Fabiano <contatodouglasfabiano1@gmail.com>
2023-11-07 23:05:00 -0300

    deleção

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -9,12 +9,14 @@ import dominio.Pessoa;
 public class Programa {
 
 	public static void main(String[] args) {
-		
+
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 
 		Pessoa p = em.find(Pessoa.class, 2);
-		System.out.println(p);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
 		
 		System.out.println("Pronto!");
 		em.close();
